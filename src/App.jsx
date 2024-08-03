4491279554e13ea727d2ef166a558fcf8e160dda
P-Asare <pinopalal11@gmail.com>
2024-08-03 17:49:35 +0000

    delete: removed all files to start from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-// import { useState } from 'react'
-// import Login from '../src/components/pages/Login'
-// import Navbar from './components/common/Navbar'
-import ForgotPasswordForm from './components/auth/ForgotPwdForm'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <ForgotPasswordForm />
+      <h1>Reset</h1>
     </>
   )
 }
