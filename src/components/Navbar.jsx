66140c59295877a2b9a895727512203575b1abf8
P-Asare <pinopalal11@gmail.com>
2024-08-03 19:50:12 +0000

    update nav bar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,17 +20,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-{/* <div className="navbar-brand">
-        <span className="bold">S</span>UPPORT<span className="bold">C</span>ENTER
-      </div>
-      <ul className="navbar-menu">
-        <li className="navbar-item">Home</li>
-        <li className="navbar-item">Find a Tutor</li>
-        <li className="navbar-item">Services</li>
-        <li className="navbar-item">About Us</li>
-      </ul>
-      <div className="navbar-buttons">
-        <button className="btn signup">Sign Up</button>
-        <button className="btn login">Login</button>
-      </div> */}
