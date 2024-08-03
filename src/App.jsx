4fbb05eaee381b84c173a0df049f2af9f90acf8b
P-Asare <pinopalal11@gmail.com>
2024-08-03 19:43:24 +0000

    add static nav-bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
       <div className="view-container">
-        <div className="main-view">"Hello world"</div>
+        <div className="main-view">
+          <Navbar />
+        </div>
       </div>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import '../styles/Navbar.css';
+import '../styles/Navbar.css'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">
+      <div className="name">Support Center</div>
+      <div className="sub-pages">
+        <div className="home">Home</div>
+        <div className="tutor">Find a tutor</div>
+        <div className="services">Services</div>
+        <div className="about">About us</div>
+      </div>
+      <div className="register-buttons">
+        <button className="sign-up">Sign Up</button>
+        <button className="login">Login</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+{/* <div className="navbar-brand">
         <span className="bold">S</span>UPPORT<span className="bold">C</span>ENTER
       </div>
       <ul className="navbar-menu">
@@ -16,9 +33,4 @@ const Navbar = () => {
       <div className="navbar-buttons">
         <button className="btn signup">Sign Up</button>
         <button className="btn login">Login</button>
-      </div>
-    </nav>
-  );
-};
-
-export default Navbar;
+      </div> */}
