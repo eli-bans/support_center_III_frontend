d261dc58bc0ab6314c98e8fb9fa0213837c9da24
P-Asare <pinopalal11@gmail.com>
2024-08-06 06:06:18 +0000

    add find tutor section of homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,6 +9,10 @@ import TechImage from "../assets/Technology.png";
 import CourseCard from "../components/homepage/CourseCard";
 import SecondMiddleImage from "../assets/Cone_04.png";
 import BackLines from '../assets/line_groups.png';
+import ServiceCard from "../components/homepage/ServiceCard";
+import BlackCalendar from "../assets/black-calendar.png";
+import ThirdTopRight from "../assets/Cone_05.png";
+import ThirdTopLeft from "../assets/Cone_06.png";
 
 
 
@@ -59,6 +63,39 @@ function Homepage () {
                     <img className="second-middle" src={SecondMiddleImage} alt="Adding image" />
                     <img className="line-groups" src={BackLines} alt="Background" />
                     
+                </div>
+            </div>
+            <div className="break-segment">
+                <button>New</button>
+                <div>We'll be adding more courses and features in v2.0 soon</div>
+            </div>
+            <div className="third-segment">
+                <div className="third-segment-content">
+                    <div className="content">
+                        <div className="col-image">
+                            <img src={BlackCalendar} alt="Image of calendar" />
+                        </div>
+                        <div className="col-info">
+                            <div className="first">
+                                <p className="title">Why you should choose us?</p>
+                                <p className="sub-title">With lots of unique blocks, you can easily build a page <br /> without coding. Build your next landing page.</p>
+                            </div>
+                            <div className="next">
+                                <ServiceCard count={1} title={"Easy Booking"} desc={"With lots of unique blocks, you can easily build a page without coding."}/>
+                                <ServiceCard count={2} title={"Free Tutor"} desc={"With lots of unique blocks, you can easily build a page without coding."}/>
+                                <ServiceCard count={3} title={"Video Conference"} desc={"With lots of unique blocks, you can easily build a page without coding."}/>
+                                <button className="tutor-button">
+                                    <div className="btn-text">
+                                        Find a Tutor
+                                    </div>
+                                    <i class="material-icons">arrow_forward</i>
+                                    </button>
+                            </div>
+
+                        </div>
+                        <img src={ThirdTopRight} alt="Cone image" className="third-right" />
+                        <img src={ThirdTopLeft} alt="Cone image" className="third-left" />
+                    </div>
                 </div>
             </div>
             
